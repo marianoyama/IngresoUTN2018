6fae469e2bb0afa3e5929ac2357757d7b564fc78
marianoyama <36230641+marianoyama@users.noreply.github.com>
2018-02-28 10:18:11 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,22 @@
-function Mostrar()
-{
+function Mostrar() {
+
+    var numero
+    var i
+    var contador = 0;
+
+    numero = parseInt(prompt("Ingrese un numero"));
+
+    for (i = 1; i <= numero; i++)
+         {
+        if (numero % i == 0) 
+        {
+            console.log(i);
+            contador++;
+        }
+    }
+console.log ("Se registraron " + contador + ("numeros divisores"));
+
+
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,23 @@
-function Mostrar()
-{
+function Mostrar() {
+    var numero
+    var i
+    var contador = 0;
+
+
+    numero = parseInt(prompt("Ingrese un numero"));
+
+    for (i = 1; i <= numero; i++) {
+        if (numero % i == 0) {
+            contador++;
+        }
+
+
+    }
+    if (contador == 2) {
+        alert("Es un numero primo");
+    }
+    else
+        alert("No es un numero primo");
 
 
 
